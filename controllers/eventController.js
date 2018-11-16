4f6115127105525661eb028e3c5d80a2b8474adf
Lucia Bianciotto <lulibian@hotmail.com>
2018-11-16 15:44:36 +0000

    index and update routes

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -2,4 +2,26 @@
 console.log('hello lucia!!!!');
 
 // Lucia
-console.log('hello Sophia!');
+function indexRoute(req, res, next) {
+  Event
+    .find()
+    .then(events =>
+      res.json(events))
+    .catch(next);
+}
+
+function updateRoute(req, res, next) {
+  Event.findById(req.params.id)
+    .then(event => event.set(req.body))
+    .then(event => event.save())
+    .then(event => res.json(event))
+    .catch(next);
+}
+
+module.exports = {
+  index: indexRoute,
+  show: showRoute,
+  create: createRoute,
+  update: updateRoute,
+  delete: deleteRoute
+};
