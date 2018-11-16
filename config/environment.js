5b87a9b8fe9f076ae87bad1ae288f2d5c07485e0
Sophia Barclay <barclay.sophia@gmail.com>
2018-11-16 15:08:55 +0000

    Folder layout

--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,5 @@
 const port = process.env.PORT || 4000;
+const dbUri = process.env.MONGODB_URI || 'mongodb://localhost/musicListing';
+const secret = process.env.SECRET || 'jusia';
 
 module.exports = { port };
