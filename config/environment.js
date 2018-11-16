8790b6d09a4766702e46d1c0434187c1e4f0ae3b
Sophia Barclay <barclay.sophia@gmail.com>
2018-11-16 15:10:27 +0000

    Environment set up

--- config/environment.js
+++ config/environment.js
@@ -2,4 +2,4 @@ const port = process.env.PORT || 4000;
 const dbUri = process.env.MONGODB_URI || 'mongodb://localhost/musicListing';
 const secret = process.env.SECRET || 'jusia';
 
-module.exports = { port };
+module.exports = { port, dbUri, secret };
