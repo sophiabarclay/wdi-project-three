16864c12ce9a7e3fa794371c096610c47a582857
Sophia Barclay <barclay.sophia@gmail.com>
2018-11-16 15:48:31 +0000

    show, create, delete routes

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,5 +1,27 @@
 // Sophia
-console.log('hello lucia!!!!');
+const Event = require('../models/event');
+
+function showRoute(req, res, next) {
+  Event
+    .findById(req.params.id)
+    // .populate('createdBy comments.User')
+    .then(event => res.json(event))
+    .catch(next);
+}
+
+function createRoute(req, res, next) {
+  Event
+    .create(req.body)
+    .then(event => res.json(event))
+    .catch(next);
+}
+
+function deleteRoute(req, res, next) {
+  Event
+    .findByIdAndDelete(req.params.id)
+    .then(() => res.sendStatus(204))
+    .catch(next);
+}
 
 // Lucia
 console.log('hello Sophia!');
