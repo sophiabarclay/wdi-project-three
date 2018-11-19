6eed5aa5d53146569003548c828186be084c3c8a
Lucia Bianciotto <lulibian@hotmail.com>
2018-11-19 10:50:38 +0000

    some changes in the index

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
+const env = require('./config/environment');
+const mongoose = require('mongoose');
+mongoose.connect(env.dbUri);
+const router = require('./config/router');
 
-const { port } = require('./config/environment');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.use('/api', router);
 
 app.use(express.static(`${__dirname}/public`));
 
 app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
-app.listen(port, () => console.log(`Express is running on port ${port}`));
+app.listen(env.port, () => console.log(`Express is running on port ${env.port}`));
 
 module.exports = app;
