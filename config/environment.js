9fbdb4ae75cee23184c80b3302bb8d3306c2d19d
Lucia Bianciotto <lulibian@hotmail.com>
2018-11-19 11:00:40 +0000

    PORT to port in env

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 const dbUri = process.env.MONGODB_URI || 'mongodb://localhost/musicListing';
 const secret = process.env.SECRET || 'jusia';
 
