ddd3e4fe0ace856827030e26fd8cfaa21e31e57a
Lucia Bianciotto <lulibian@hotmail.com>
2018-11-22 12:01:04 +0000

    register error

--- src/controllers/registerCtrl.js
+++ src/controllers/registerCtrl.js
@@ -1,9 +1,13 @@
-function registerCtrl($scope, $state, $auth) {
+function registerCtrl($scope, $state, $auth, Flash) {
   $scope.handleRegister= function() {
     console.log('Registering user!');
     $auth.signup($scope.user)
       .then(() => $state.go('login'))
-      .catch(err => console.log('There was an error registering', err));
+      .catch(err => {
+        console.log('There was an error registering', err);
+        Flash
+          .create('danger', 'Password does not match all fields. Must contain uppercase, lowercase and numeric characters, as well as being at least 8 characters long.');
+      });
   };
 }
 
