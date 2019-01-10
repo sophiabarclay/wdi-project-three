a722b0bc6c67dc688b4bda438527dbc79758938f
Sophia Barclay <barclay.sophia@gmail.com>
2019-01-10 16:21:38 +0000

    deployment

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,6 @@
 const port = process.env.PORT || 4000;
-const dbUri = process.env.MONGODB_URI || 'mongodb://localhost/musicListing';
+const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/gighub';
 const secret = process.env.SECRET || 'jusia';
+
 
 module.exports = { port, dbUri, secret };
